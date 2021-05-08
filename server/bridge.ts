02aa4bc006c57758430a36adaebfe584ef7920d3
jimmyruann <jimmyruan@hotmail.com>
2021-05-08 23:37:18 +1000

    set pythoonpath

--- server/bridge.ts
+++ server/bridge.ts
@@ -1,22 +1,39 @@
 import so from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
-import { PythonShell } from "python-shell";
+import { Options, PythonShell } from "python-shell";
 import path from "path";
 
 export async function bridge(io: so.Server<DefaultEventsMap, DefaultEventsMap>, fileDir: string, classifier: string) {
-	const pyshell = new PythonShell("run_classification.py", {
-		mode: "text",
-		pythonOptions: ["-u"],
-		scriptPath: path.join(process.cwd(), "/cnn"),
-		args: [
-			"-m",
-			path.join(process.cwd(), "/cnn/models", `${classifier}.h5`),
-			"-i",
-			path.join(process.cwd(), "/cnn/input", fileDir),
-			"-o",
-			path.join(process.cwd(), "/cnn/output", fileDir),
-		],
-	});
+	const options: Options = process.env.PYTHON_PATH
+		? {
+				pythonPath: process.env.PYTHON_PATH,
+				mode: "text",
+				pythonOptions: ["-u"],
+				scriptPath: path.join(process.cwd(), "/cnn"),
+				args: [
+					"-m",
+					path.join(process.cwd(), "/cnn/models", `${classifier}.h5`),
+					"-i",
+					path.join(process.cwd(), "/cnn/input", fileDir),
+					"-o",
+					path.join(process.cwd(), "/cnn/output", fileDir),
+				],
+		  }
+		: {
+				mode: "text",
+				pythonOptions: ["-u"],
+				scriptPath: path.join(process.cwd(), "/cnn"),
+				args: [
+					"-m",
+					path.join(process.cwd(), "/cnn/models", `${classifier}.h5`),
+					"-i",
+					path.join(process.cwd(), "/cnn/input", fileDir),
+					"-o",
+					path.join(process.cwd(), "/cnn/output", fileDir),
+				],
+		  };
+
+	const pyshell = new PythonShell("run_classification.py", options);
 
 	pyshell.on("message", function (message) {
 		io.emit("python", message);
